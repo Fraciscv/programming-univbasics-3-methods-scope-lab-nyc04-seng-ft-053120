a82380984d039b3a7482ab34c5b2d1658fad9f64
Francisco Andaur <francisco.andaur@icloud.com>
2020-06-04 14:18:35 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,15 +1,16 @@
+$danger = "It's Dangerous To Go Alone! Take This."
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+$phrase = "It's-a me, Mario!"
+
 def mario
-  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
 def link
-  danger = "It's Dangerous To Go Alone! Take This."
   puts danger
 end
 
